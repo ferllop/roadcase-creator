e2e3d0d8c19d2ee59a410f61f4cae68c2b75cfcc
Ferran <fllop@ctspain.com>
2019-04-10 09:29:08 +0200

    Add project files.

---
+++ Autotyper 1.0/Form1.vb
@@ -0,0 +1,43 @@
+﻿Public Class Form1
+    Sub Button1_Click() Handles Button1.Click
+        Delay(5)
+        Dim okroadcase As String
+        Dim nuevoroadcase As String
+        Dim tick As String
+        Dim linea As String()
+        Dim codigos As String
+        Dim i As Integer
+
+        i = 0
+        codigos = TextBox1.Text
+        linea = Split(codigos, vbCrLf)
+        okroadcase = "+{TAB 2}{ENTER}"
+        nuevoroadcase = "{HOME}{DOWN}{TAB}{ENTER}{TAB 2}{ENTER}"
+        tick = "{Enter}{TAB}{RIGHT} {LEFT 2}"
+
+        For i = 0 To UBound(linea)
+            If i = UBound(linea) Then
+                SendKeys.Send(linea(i))
+            Else
+                If linea(i).Contains(".RC") Then
+                    SendKeys.Send(linea(i))
+                    SendKeys.Send(tick)
+                Else
+                    If linea(i + 1).Contains(".RC") Then
+                        SendKeys.Send(linea(i) & "{Enter}")
+                        SendKeys.Send(okroadcase & "{Enter}")
+                        Delay(2)
+                        SendKeys.Send(nuevoroadcase)
+                    Else
+                        SendKeys.Send(linea(i) & "{Enter}")
+                    End If
+                End If
+            End If
+        Next i
+
+        Me.TopMost = True
+        MessageBox.Show("El proceso ha terminado")
+
+    End Sub
+End Class
+
---
+++ Autotyper 1.0/Form1conCheckDeVentana.Designer.vb
@@ -0,0 +1,125 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(230, 87)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Start"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper
+        Me.TextBox1.Location = New System.Drawing.Point(27, 134)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both
+        Me.TextBox1.Size = New System.Drawing.Size(171, 521)
+        Me.TextBox1.TabIndex = 1
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"Absen (6)", "Absen Back Support (6)", "Absen Ground Beam 1 x 1.5m + 2 x 1m(4) ", "Absen Ground Beam 2 x 2m (4)", "24 x DigiLED MCK7 (4)", "21K (2)", "13K (2)", "10K (2)", "8.5K (2)", "85"" Mate o Brillo (4)", "80"" (4)", "1 x 65"" (4)", "2 x 65"" (4)", "3 x 65"" (4)", "2 x 55"" 4K QM55H (4)", "3 x 55"" 4K QM55H (4)", "2 x 55"" ME55C (4)", "2 x 55"" DB55D (4)", "2 x 49"" QB49N (4)", "3 x 49"" QB49N (4)", "1 x 48"" (4)", "2 x 48"" (4)", "2 x 46"" (4)", "Clevertouch 55"" (4)", "NEC X55 (5)", "4 x T10 (8)", "MAUI (4)", "QL5 (4)", "Axiom (6)", "MAC700 (6)", "Z8 Strip (4)", "Socapex (1)"})
+        Me.ComboBox1.Location = New System.Drawing.Point(27, 87)
+        Me.ComboBox1.MaxDropDownItems = 10
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(171, 21)
+        Me.ComboBox1.TabIndex = 2
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(241, 13)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Selecciona el tipo de paquete y selecciona Start. "
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 22)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(293, 13)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Tendrás 5s para seleccionar la ventana 'Pack a Road case'."
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(24, 58)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(155, 13)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Selecciona un tipo de paquete:"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(12, 71)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(192, 13)
+        Me.Label4.TabIndex = 6
+        Me.Label4.Text = "(entre paréntesis el límite de paquetes.)"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(319, 667)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Roadcase Creator 2000"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents ComboBox1 As System.Windows.Forms.ComboBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+End Class
